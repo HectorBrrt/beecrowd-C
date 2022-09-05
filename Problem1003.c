cafa9d35975510cb1b8ab6a6e6009da729a379ff
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-05 17:23:52 -0300

    Update Problem1003.c

--- Problem1003.c
+++ Problem1003.c
@@ -1,11 +1,28 @@
 /*
 Simple Sum
 Read two integer values, in this case, the variables A and B. 
-After this, calculate the sum between them and assign it to the variable SOMA. 
+After this, calculate the sum between them and assign it to the variable SUM. 
 Write the value of this variable.
 Input
 The input file contains 2 integer numbers.
 Output
-Print the message "SOMA" with all the capital letters, 
+Print the message "SUM" with all the capital letters, 
 with a blank space before and after the equal signal followed by the corresponding value to the sum of A and B. 
 */
+
+#include <stdio.h>
+
+int main() {
+    int A,B;
+
+    printf("A: ");
+    scanf("%d", &A);
+
+    printf("B: ");
+    scanf("%d", &B);
+
+    int SUM = A+B;
+
+    printf("X: %d", SUM);
+
+}
