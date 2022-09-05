188bc67459892d771810f0ecfdfbdac2c35cdbce
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-05 17:27:13 -0300

    Update and rename Problem1004.java to Problem1004.c

--- Problem1004.c
+++ Problem1004.c
@@ -6,3 +6,20 @@ The input file contains 2 integer numbers.
 Output
 Print the message "PROD" and PROD according to the following example, with a blank space before and after the equal signal.
 */
+
+#include <stdio.h>
+
+int main() {
+    int n1,n2;
+
+    printf("A: ");
+    scanf("%d", &n1);
+
+    printf("B: ");
+    scanf("%d", &n2);
+
+    int PROD = n1*n2;
+
+    printf("PROD = %d", PROD);
+
+}
