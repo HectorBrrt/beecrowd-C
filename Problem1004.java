c449b520b526c4e395c2896744762aca5c94de75
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-05 17:25:35 -0300

    Create Problem1004.java

---
+++ Problem1004.java
@@ -0,0 +1,8 @@
+/*
+Simple Product
+Read two integer values. After this, calculate the product between them and store the result in a variable named PROD. 
+Input
+The input file contains 2 integer numbers.
+Output
+Print the message "PROD" and PROD according to the following example, with a blank space before and after the equal signal.
+*/
