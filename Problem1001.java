2f2038e177ccc31e409e521a29bf1a679f1f7650
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-05 16:54:01 -0300

    Create Problem1001.java

---
+++ Problem1001.java
@@ -0,0 +1,10 @@
+/*
+Extremely Basic
+Read 2 variables, named A and B and make the sum of these two variables, assigning its result to the variable X. 
+Print X as shown below. Print endline after the result otherwise you will get “Presentation Error”.
+Input
+The input file will contain 2 integer numbers.
+Output
+Print the letter X (uppercase) with a blank space before and after the equal signal followed by the value of X, 
+according to the following example.
+*/
