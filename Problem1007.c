30d93848dac8b270ba552ced534ff9a1367f7533
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-06 22:20:16 -0300

    Create Problem1007.c

---
+++ Problem1007.c
@@ -0,0 +1,10 @@
+/*
+Difference:
+Read four integer values named A, B, C and D. 
+Calculate and print the difference of product A and B by the product of C and D (A * B - C * D).
+Input
+The input file contains 4 integer values.
+Output
+Print DIFERENCA (DIFFERENCE in Portuguese) with all the capital letters, 
+according to the following example, with a blank space before and after the equal signal.
+*/
