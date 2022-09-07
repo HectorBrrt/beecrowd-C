9568065a8e99e3fb91aabc6ae057645c2666d35b
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-06 22:17:25 -0300

    Create Problem1006.c

---
+++ Problem1006.c
@@ -0,0 +1,14 @@
+/*
+Average 2:
+Read three values (variables A, B and C), which are the three student's grades.
+Then, calculate the average, considering that grade A has weight 2, grade B has weight 3 and the grade C has weight 5.
+Consider that each grade can go from 0 to 10.0, always with one decimal place.
+
+Input
+The input file contains 3 values of floating points (double) with one digit after the decimal point.
+
+Output
+Print the message "MEDIA"(average in Portuguese) and the student's average according to the following example,
+with a blank space before and after the equal signal.
+*/
+
