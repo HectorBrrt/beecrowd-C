652a623ebc3a38017b8b2fb39b5d8d83cb081737
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-06 22:36:04 -0300

    Update Problem1009.c

--- Problem1009.c
+++ Problem1009.c
@@ -1,1 +1,7 @@
-
+/*
+Salary:
+Make a program that reads a seller's name, his/her fixed salary and the sale's total made by himself/herself in the month (in money).
+Considering that this seller receives 15% over all products sold,
+write the final salary (total) of this seller at the end of the month , with two decimal places.
+- Don’t forget to print the line's end after the result, otherwise you will receive “Presentation Error”.
+- Don’t forget the blank spaces.
