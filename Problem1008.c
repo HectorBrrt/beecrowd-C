a2364e70f90d66388c3b4efbae7a7b1ba43f0d3a
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-06 22:25:46 -0300

    Update Problem1008.c

--- Problem1008.c
+++ Problem1008.c
@@ -3,9 +3,11 @@ Salary:
 Write a program that reads an employee's number, his/her worked hours number in a month and the amount he received per hour.
 Print the employee's number and salary that he/she will receive at end of the month, with two decimal places.
 Don’t forget the space before and after the equal signal and after the U$.
+
 Input
 The input file contains 2 integer numbers and 1 value of floating point, representing the number,
 worked hours amount and the amount the employee receives per worked hour.
+
 Output
 Print the number and the employee's salary, according to the given example, with a blank space before and after the equal signal.
 */
