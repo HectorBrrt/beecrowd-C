316ec4d4159fae4220117303fd9f25756e34af80
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-08 20:49:57 -0300

    Update Problem1010.c

--- Problem1010.c
+++ Problem1010.c
@@ -1,1 +1,16 @@
+/*
+Simple Calculate:
+In this problem, the task is to read a
+code of a product 1, the number of units of product 1,the price for one unit of product 1,
+the code of a product 2,the number of units of product 2 and the price for one unit of product 2.
+After this, calculate and show the amount to be paid.
+
+Input
+The input file contains two lines of data. In each line there will be 3 values:
+two integers and a floating value with 2 digits after the decimal point.
+
+Output
+The output file must be a message like the following example where "Valor a pagar" means Value to Pay.
+Remember the space after ":" and after "R$" symbol. The value must be presented with 2 digits after the point.
+*/
 
