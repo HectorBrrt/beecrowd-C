931da49d1ad53efd41234a404d14075e830bff59
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-11 16:34:46 -0300

    Update Problem1004.c

--- Beginner/1000-1019/Problem1004.c
+++ Beginner/1000-1019/Problem1004.c
@@ -1,8 +1,10 @@
 /*
 Simple Product
 Read two integer values. After this, calculate the product between them and store the result in a variable named PROD. 
+
 Input
 The input file contains 2 integer numbers.
+
 Output
 Print the message "PROD" and PROD according to the following example, with a blank space before and after the equal signal.
 */
@@ -20,5 +22,4 @@ int main() {
     int PROD = n1*n2;
 
     printf("PROD = %d", PROD);
-
 }
