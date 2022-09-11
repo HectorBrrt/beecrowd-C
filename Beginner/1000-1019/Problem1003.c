8f630891d2e104fc22ff1dde9c5d127000f688f2
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-11 16:34:13 -0300

    Update Problem1003.c

--- Beginner/1000-1019/Problem1003.c
+++ Beginner/1000-1019/Problem1003.c
@@ -3,8 +3,10 @@ Simple Sum
 Read two integer values, in this case, the variables A and B. 
 After this, calculate the sum between them and assign it to the variable SUM. 
 Write the value of this variable.
+
 Input
 The input file contains 2 integer numbers.
+
 Output
 Print the message "SUM" with all the capital letters, 
 with a blank space before and after the equal signal followed by the corresponding value to the sum of A and B. 
