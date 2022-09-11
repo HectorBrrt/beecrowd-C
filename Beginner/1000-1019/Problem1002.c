6c2dd4c2417a52a9c9c9e3a225ebd8820675f3d3
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-11 16:34:22 -0300

    Update Problem1002.c

--- Beginner/1000-1019/Problem1002.c
+++ Beginner/1000-1019/Problem1002.c
@@ -2,8 +2,10 @@
 Area of a Circle
 The formula to calculate the area of a circumference is defined as A = π . R2. Considering to this problem that π = 3.14159:
 Calculate the area using the formula given in the problem description.
+
 Input
 The input contains a value of floating point (double precision), that is the variable R.
+
 Output
 Present the message "A=" followed by the value of the variable, 
 as in the example bellow, with four places after the decimal point. Use all double precision variables. 
