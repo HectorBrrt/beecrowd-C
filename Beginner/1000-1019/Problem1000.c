c5d9c6953e73ea820bd4737ae28fdfc8afbc433b
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-17 16:42:06 -0300

    Update Problem1000.c

--- Beginner/1000-1019/Problem1000.c
+++ Beginner/1000-1019/Problem1000.c
@@ -6,6 +6,7 @@ In this first problem all you have to do is print this message on the screen.
 
 Input
 This problem has no input.
+
 Output
 You must print the message Hello World! and then the endline as shown below.
 */
