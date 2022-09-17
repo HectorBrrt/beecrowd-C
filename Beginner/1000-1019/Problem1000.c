314eb01500a82927d1e56bff9b14032f239202aa
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-17 16:41:57 -0300

    Update Problem1000.c

--- Beginner/1000-1019/Problem1000.c
+++ Beginner/1000-1019/Problem1000.c
@@ -3,6 +3,7 @@ Hello World!
 Welcome to beecrowd!
 Your first program in any programming language is usually "Hello World!". 
 In this first problem all you have to do is print this message on the screen.
+
 Input
 This problem has no input.
 Output
