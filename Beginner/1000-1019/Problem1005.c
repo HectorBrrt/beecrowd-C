bf5bca364779938cfd1629f9b89641a4b6c79658
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-17 16:45:19 -0300

    Update Problem1005.c

--- Beginner/1000-1019/Problem1005.c
+++ Beginner/1000-1019/Problem1005.c
@@ -1,5 +1,5 @@
 /*  
-Average 1:
+Average 1: 
 Read two floating points' values of double precision A and B, corresponding to two student's grades. 
 After this, calculate the student's average, considering that grade A has weight 3.5 and B has weight 7.5. 
 Each grade can be from zero to ten, always with one digit after the decimal point. 
